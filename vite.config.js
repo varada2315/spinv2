09126556b2c7194dbd53580c69715cf8ce7c8da6
root <root@srv1791376.hstgr.cloud>
2026-07-28 09:52:09 +0000

    spinss

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    host: true
+    port: 6041,
+    host: '0.0.0.0',
+    allowedHosts: ['spinv2.cyberpunk.co.in']
   }
 })
